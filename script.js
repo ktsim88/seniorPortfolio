acac0873a4dd6cf3279fef6ee3cbb270ea15117a
ktsim88 <526ktsim@frhsd.com>
2026-05-11 17:48:21 +0000

    added vue app

--- script.js
+++ script.js
@@ -2,6 +2,22 @@
 const myModal = document.getElementById('myModal')
 const myInput = document.getElementById('myInput')
 
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
+// myModal.addEventListener('shown.bs.modal', () => {
+//   myInput.focus()
+// })
+
+
+const app = Vue.createApp({
+  created() { 
+    fetch('zeronis.json').then(rep => rep.json()).then(json => {
+      this.zeroniIcons = json;
+     })
+  },
+  data() {
+    return {
+      zeroniIcons: [],
+    }
+   }
 })
+
+app.mount('#app')
